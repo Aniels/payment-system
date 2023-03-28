1bece62df63bd5239a2b9aa86880878dc795aa92
Aniels <96707275+Aniels@users.noreply.github.com>
2023-03-28 22:57:33 +0100

    user model setup

--- payapp/models.py
+++ payapp/models.py
@@ -1,4 +1,19 @@
 from django.db import models
+from django.utils.timezone import now
+
 
 # Create your models here.
+class Record(models.Model):
+
+    payment_method = (
+        ('T', 'transform'),
+        ('C', 'credit'),
+    )
+
+    time_stamp = models.DateTimeField(default=now, blank=True)
+    amount = models.FloatField()
+    method = models.CharField(max_length=1, choices=payment_method)  # Not null
+
+
+
 
